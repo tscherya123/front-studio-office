6b1ea341dc4963e89621bf1ad85069a7a7f8b9f7
tscherya123 <tscherya123@gmail.com>
2021-06-12 14:21:58 +0300

    TicketSystem

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import UserPage from './pages/panel/UserPage';
 import EngineerPage from './pages/panel/EngineerPage';
 import PanelPage from './pages/panel/PanelPage';
 import ProfilePage from './pages/panel/ProfilePage';
+import MyTicketsPage from './pages/panel/MyTicketsPage';
+import FreeTicketsPage from './pages/panel/FreeTicketsPage';
+import TicketsPage from './pages/panel/TicketsPage';
+import CreateTicketPage from './pages/panel/CreateTicketPage';
+import CreateTicketUpdater from './pages/panel/CreateTicketUpdater';
+import AllTicketsPage from './pages/panel/AllTicketsPage';
+import AllUsersPage from './pages/panel/AllUsersPage';
 import { USER, ADMIN, ENGINEER } from './components/util/accessTypes';
 
 class App extends Component {
@@ -21,14 +28,19 @@ class App extends Component {
         <Router>
           <div className="App">
             <Switch>
-              <Route exact path='/' component={Landing}/>
-              <Route path="/welcome" component={Landing}/>
+              <Route exact path='/' component={SignInSide}/>
+              <Route path="/welcome" component={SignInSide}/>
               <Route path="/sign-in" component={SignInSide}/>
               <Route path="/sign-up" component={SignUpPage}/>
               <Route path="/checkAccess" component={PanelPage}/>
-              <Route path="/admin/profile" component={ProfilePage} />
-              <Route path="/user/profile" component={ProfilePage} />
-              <Route path="/engineer/profile" component={ProfilePage} />
+              <Route path="/profile" component={ProfilePage} />
+              <Route exact path="/myTickets" component={MyTicketsPage} />
+              <Route exact path="/freeTickets" component={FreeTicketsPage} />
+              <Route exact path="/allTickets" component={AllTicketsPage} />
+              <Route exact path="/allUsers" component={AllUsersPage} />
+              <Route path="/createTicket" component={CreateTicketPage} />
+              <Route exact path="/anotherTicket" component={CreateTicketUpdater} />
+              <Route path="/ticket/:id" component={TicketsPage} />
             </Switch>
           </div>
         </Router>
